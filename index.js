d50d77171e547d580ddc74d5479eb46258319700
Caleb Forestal <133164966+Calebrlx@users.noreply.github.com>
2023-11-05 17:56:02 -0700

    mmm

--- index.js
+++ index.js
@@ -1,12 +1,101 @@
 const http = require('http');
 const PORT = 3000;
+const Twitter = require('twitter');
+require('dotenv').config();
+import OpenAI from 'openai';
+
+const client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+const openai = new OpenAI({
+apiKey: process.env.OPENAI_API_KEY,
+});
+
+function postTweet(status) {
+  client.post('statuses/update', {status}, function(error, tweet, response) {
+    if (!error) {
+      console.log("Successfully tweeted: " + tweet.text);
+    } else {
+      console.error("Failed to tweet. Error:", error);
+      if (response) {
+        console.log("Response statusCode:", response.statusCode);
+        console.log("Response body:", response.body);
+      }
+    }
+  });
+}
+
+async function GPT() {
+    const chatCompletion = await openai.chat.completions.create({
+        messages: [{ role: 'system', content: 'You are an ai responcible for running Relixs (AI Service provider) twitter page. Come up with a creative and unique tweet inorder to gain followers and gain brand awarness. your tweets should be aimed at the demograpic of 18 to 24 year olds who may not be too familiar with the latest ai tech. each tweet should be quality, engaging, interesting and professional.' }, { role: 'user', content: 'Write 5 tweets' }],
+        model: 'gpt-3.5-turbo',
+    });
+    console.log(chatCompletion)
+}
+
+// Post a tweet
+// postTweet('Hello World! My name is Relix');
+
+
+// Random time generator function based on current time
+function generateRandomTime() {
+  const currentHour = new Date().getHours();
+  let randomHour, randomMinute, randomSecond;
+
+  // If current time is before 3 PM, schedule for current day
+  // Otherwise, schedule for next day if it's past 10 PM
+  let scheduleDate = new Date();
+  if (currentHour >= 15 && currentHour < 19) {
+    // Choose a time between 7 PM - 10 PM if it's past 3 PM
+    randomHour = Math.floor(Math.random() * 3 + 19);
+  } else {
+    // Choose a time between 12 PM - 3 PM
+    if (currentHour >= 10) {
+      // If it's past 10 AM, schedule for the next day
+      scheduleDate.setDate(scheduleDate.getDate() + 1);
+    }
+    randomHour = Math.floor(Math.random() * 3 + 12);
+  }
+  randomMinute = Math.floor(Math.random() * 60);
+  randomSecond = Math.floor(Math.random() * 60);
+
+  // Set the random time for execution
+  scheduleDate.setHours(randomHour, randomMinute, randomSecond, 0);
+
+  return scheduleDate;
+}
+
+// Schedule a function to run at the random time
+function scheduleNextAction() {
+  const nextTime = generateRandomTime();
+
+  const delay = nextTime.getTime() - new Date().getTime();
+  if (delay < 0) {
+    // If for any reason the delay is negative, schedule immediately
+    setImmediate(customLogic);
+  } else {
+    console.log(`Next action scheduled for: ${nextTime}`);
+    setTimeout(customLogic, delay);
+  }
+}
+
+// Place your custom logic here
+function customLogic() {
+  GPT()
+  scheduleNextAction();
+}
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+  res.end('Hello World! Scheduled logic will execute as per random time set.');
 });
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
+  scheduleNextAction(); 
 });
