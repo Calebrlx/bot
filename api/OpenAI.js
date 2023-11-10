62235842d48ff56fb82ffdf532f4fc98566e8101
Caleb Forestal <133164966+Calebrlx@users.noreply.github.com>
2023-11-10 15:57:12 -0700

    p

--- api/OpenAI.js
+++ api/OpenAI.js
@@ -27,7 +27,7 @@ async function tweetIdeas() {
 
         // Parse the ideas into a format that can be concatenated into a follow-up message
         const ideaContent = ideasResponse.choices[0].message.content;
-        return ideasContent; 
+        return ideasContent;
     } catch (error) {
         console.error("Error getting tweet ideas:", error);
         throw error; 
